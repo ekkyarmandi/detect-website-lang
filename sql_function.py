cd9a52654e43c7f3029537062dc08fdc7fa7836f
Ekky Armandi <ekkyarmandi@gmail.com>
2022-05-27 22:57:46 +0700

    adding dictionary values into one line string

--- sql_function.py
+++ sql_function.py
@@ -3,36 +3,45 @@ from pandas import DataFrame
 import sqlite3
 import json
 
+def value2str(dict):
+    list = []
+    for value in dict.values():
+        if type(value) == str and value != "NULL" and "'" not in value:
+            value = "'" + value + "'"
+        if type(value) == str and value != "NULL":
+            value = '"' + value + '"'
+        elif value == None:
+            value = "None"
+        list.append(value)
+    return ",".join(list)
+
 def create_table(table: str, field: dict) -> None:
     field = [key+" "+value for key,value in field.items()]
     field = ",".join(field)
     with sqlite3.connect("data/websites.db") as con:
         cur = con.cursor()
-        cmd = f"""CREATE TABLE {table} (
-            {field}
-        )
-        """
+        cmd = f"""CREATE TABLE {table} ({field})"""
         cur.execute(cmd)
         con.commit()
 
-def insert(entries: dict, table: str = "websites"):
-    n = len(entries)
-    columns = ",".join([*entries])
-    value = ",".join(["?" for _ in range(n)])
-    values = [*entries.values()]
-    with sqlite3.connect("data/websites.db") as con:
-        cur = con.cursor()
-        cmd = f"""INSERT or IGNORE into {table} ({columns}) VALUES ({value})"""
-        cur.execute(cmd,(*values,))
-        con.commit()
+def insert(entry: dict, table: str = "websites"):
+    columns = ",".join([*entry])
+    values = value2str(entry)
+    try:
+        with sqlite3.connect("data/websites.db") as con:
+            cur = con.cursor()
+            cmd = f"INSERT or IGNORE into {table} ({columns}) VALUES ({values})"
+            cur.execute(cmd)
+            con.commit()
+    except:
+        with open("data/failed.txt","a",encoding="utf-8") as f:
+            f.write(str(entry)+"\n")
 
 def update(entry: dict, table: str = "websites"):
-    for k,v in entry.items():
-        entry[k] = v if v!=None else "None"
     values = ",".join([f"{k}='{v}'" for k,v in entry.items() if k!='url'])
     with sqlite3.connect("data/websites.db") as con:
         cur = con.cursor()
-        cmd = f"""UPDATE {table} SET {values} WHERE url=?"""
+        cmd = f"UPDATE {table} SET {values} WHERE url=?"
         cur.execute(cmd,(entry['url'],))
         con.commit()
 
@@ -44,7 +53,7 @@ def query_all(table: str = "websites"):
         queries = cur.fetchall()
     return [j[0] for j in queries]
 
-def to_csv(filename: str, table: str = "websites"):
+def export(filename: str, table: str = "websites"):
     with sqlite3.connect("data/websites.db") as con:
         cur = con.cursor()
         cmd = f"SELECT url,lang FROM {table};"
@@ -52,7 +61,10 @@ def to_csv(filename: str, table: str = "websites"):
         queries = cur.fetchall()
         dataframe = DataFrame(queries)
         dataframe.columns = ["url","language"]
-        dataframe.to_csv(filename, index=False)
+        if filename.endswith("csv"):
+            dataframe.to_csv(filename, index=False)
+        elif filename.endswith("xlsx"):
+            dataframe.to_excel(filename, index=False)
 
 
 if __name__ == "__main__":
@@ -72,4 +84,4 @@ if __name__ == "__main__":
     #     insert(dict(url=url,lang=None))
 
     # query test
-    to_csv(filename="example_5000websites.csv")
+    export(filename="output/example_5000websites.xlsx")
